b55f06cf27a4f2bd90c317f1f4a0eac43a047927
Parvesh <parveshahamed00@gmail.com>
2022-10-16 13:50:46 +0530

    adding success page, when student successfully added to database this success page will appear in our application

--- controllers/add-student-post.js
+++ controllers/add-student-post.js
@@ -1,25 +1,26 @@
 const studentschema = require("../models/student-detail");
 
 exports.addStudent = (req, res) => {
-    const student = new studentschema.student_data({
-      name: req.body.name,
-      admissionNo: req.body.admissionNo,
-      rollNo: req.body.rollNo,
-      email: req.body.email,
-      phoneNo: req.body.phoneNo,
-      age: req.body.age,
-      gender: req.body.gender,
-      department: req.body.department,
-      address: req.body.address,
-      fees: req.body.fees,
-    });
-    student.save(function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Scuessfully Saved to database");
-      }
-    });
+  const student = new studentschema.student_data({
+    name: req.body.name,
+    admissionNo: req.body.admissionNo,
+    rollNo: req.body.rollNo,
+    email: req.body.email,
+    phoneNo: req.body.phoneNo,
+    age: req.body.age,
+    gender: req.body.gender,
+    department: req.body.department,
+    address: req.body.address,
+    fees: req.body.fees,
+  });
+  student.save(function (err, result) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log("Scuessfully Saved to database");
+      res.sendFile(__dirname+"/add-success.html")
+    }
+  });
   // console.log(req.body);
-  
 };
