025ca3b4268247e676dd1acf80fbdfde152eb4f7
Parvesh <parveshahamed00@gmail.com>
2022-10-26 20:25:06 +0530

    finish work on altering student

--- controllers/alter-student-post.js
+++ controllers/alter-student-post.js
@@ -1,25 +1,55 @@
 const studentschema = require("../models/student-detail");
 
 exports.alterStudent = (req, res) => {
-    const datas=[]
-    if(req.body.name){
-        datas.push({ name: req.body.name });
+  const datas = {};
+  if (req.body.name) {
+    datas.name = req.body.name;
+  }
+  if (req.body.rollNo) {
+    datas.rollNo = req.body.rollNo;
+  }
+  if (req.body.email) {
+    datas.email = req.body.email;
+  }
+  if (req.body.phoneNo) {
+    datas.phoneNo = req.body.phoneNo;
+  }
+  if (req.body.age) {
+    datas.age = req.body.age;
+  }
+  if (req.body.gender) {
+    datas.gender = req.body.gender;
+  }
+  if (req.body.department) {
+    datas.department = req.body.department;
+  }
+  if (req.body.address) {
+    datas.address = req.body.address;
+  }
+  if (req.body.fees) {
+    datas.fees = req.body.fees;
+  }
+  // console.log(datas);
+  const admissionNo = req.body.alter_admissionNo;
+  studentschema.student_data.findOneAndUpdate(
+    { admissionNo: admissionNo },
+    datas,
+    (err, docs) => {
+      if (!err) {
+        // console.log("Student Successfully Altered");
+        // console.log(docs);
+        if (docs === null) {
+          res.write("<h1>Error !!</h1>");
+          res.write("<h3>Student Not found in database</h3>");
+
+          console.log("Student Not fount in database");
+        } else {
+          console.log("student successfully altered in database");
+          res.sendFile(__dirname + "/alter-student-success.html");
+        }
+      } else {
+        console.log(err);
+      }
     }
-//   const admissionNo = req.body.alter_admissionNo;
-//   studentschema.student_data.findOneAndUpdate(
-//     { admissionNo: admissionNo },
-
-//     { name: req.body.name, rollNo: req.body.rollNo },
-//     { ignoreUndefined: 1 },
-
-//     (err, docs) => {
-//       if (!err) {
-//         console.log("Student Successfully Altered");
-//         console.log(docs);
-//       } else {
-//         console.log(err);
-//       }
-//     }
-//   );
-  // console.log(admissionNo);
+  );
 };
